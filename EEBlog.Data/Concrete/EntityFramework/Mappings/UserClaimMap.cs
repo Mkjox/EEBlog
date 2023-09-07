0217ccdc957563a53c4db15df3e0a8bea6a2c3be
jox <zincir_890@hotmail.com>
2023-09-07 08:13:26 +0300

    Added Mappings

---
+++ EEBlog.Data/Concrete/EntityFramework/Mappings/UserClaimMap.cs
@@ -0,0 +1,23 @@
+﻿using EEBlog.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class UserClaimMap : IEntityTypeConfiguration<UserClaim>
+    {
+        public void Configure(EntityTypeBuilder<UserClaim> builder)
+        {
+            //Primary Key
+            builder.HasKey(uc => uc.Id);
+
+            //Maps to the AspNetUserClaims table
+            builder.ToTable("UserClaims");
+        }
+    }
+}
