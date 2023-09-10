e92f43d1dbabbb6e60511293bb5fb2aa708a1451
jox <zincir_890@hotmail.com>
2023-09-10 19:21:01 +0300

    Completed missing Classes

--- EEBlog.Entities/Concrete/Comment.cs
+++ EEBlog.Entities/Concrete/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EEBlog.Shared.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace EEBlog.Entities.Concrete
 {
-    public class Comment
+    public class Comment : EntityBase, IEntity
     {
+        public string Text { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
     }
 }
--- EEBlog.Shared/Entities/Abstract/EntityBase.cs
+++ EEBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -10,7 +10,7 @@ namespace EEBlog.Shared.Entities.Abstract
     {
         public virtual int Id { get; set; }
         public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
-        public virtual DateTime ModifiedDate { get;} = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
         public virtual bool IsDeleted { get; set; } = false;
         public virtual bool IsActive { get; set; } = true;
         public virtual string CreatedByName { get; set; } = "Admin";
